fa2d1249f2600cc87889b6b44abe99518a0a7aa4
srikanth.allu@gmail.com <srikanth.allu@gmail.com@6ec305e3-af67-96d2-1837-8ffe4f4433d4>
2013-06-20 21:02:49 +0000

    making test do MMS

--- driver.m
+++ driver.m
@@ -1,4 +1,7 @@
 
+clc;
+clear all;
+
 %Number of time steps
 N = 10; 
 
@@ -23,24 +26,32 @@ Tinf = 300;
 %Initial Temperature
 Tinit = 300*ones(M, 1);
 
-%Observations: Taken at x = 0 and all time > 0.
-Tstar = zeros(1, N);
-for p = 1:N
-    Tstar(p) = 300 + (p*2.0);
-end
-
 %Specific Heat (includes density factor)
 cp = [1.0; 1.0; 1.0];
 
 %Diffusion Coefficient (Linear Model: K = Ka + Kb*t)
 Ka = [10; 10; 10];
-Kb = [0; 0; 0];
+Kb = [2; 3; 4]; %MMS solution
 
 elMmat = elemMmat();
 elKmat = elemKmat();
 
 %Mass matrix
 Bmat = formBmat(elMmat, M, Ne, h, dt, cp);
+
+T = solveForward(Bmat, elKmat, M, N, Ne, h, H, Ka, Kb, Tinf, Tinit, dt);
+
+% %Observations: Taken at x = 0 and all time > 0.
+% Tstar = zeros(1, N);
+% for p = 1:N
+%     Tstar(p) = 300 + (p*2.0);
+% end
+
+Tstar = T(1, :);
+
+
+%Initial guess
+Kb = [0; 0; 0];
 
 %Approximate Inverse of Hessian Matrix
 invHess = eye(3, 3);
@@ -51,23 +62,27 @@ maxOptIters = 100;
 dfdG1vec = zeros(M, N);
 dfdG2vec = zeros(M, N);
 dfdG3vec = zeros(M, N);
+% STEP 2 for computing T at all time steps
 T = solveForward(Bmat, elKmat, M, N, Ne, h, H, Ka, Kb, Tinf, Tinit, dt);
 deltaT = T(1, :) - Tstar;
 obj = sum(deltaT.^2);
 if obj <= 1.0e-12
    disp('Objective function value is below the tolerance!');
 else
+    % STEP 3 solve Adjoint for Lambda, Lvec
    Lvec = solveAdjoint(T, Tstar, Bmat, elKmat, M, N, Ne, h, H, Ka, Kb, dt);
    for p = 1:N
       dfdG1vec(:, p) = formDFDGvec(elKmat, M, Ne, h, T, dt, p, 1);
       dfdG2vec(:, p) = formDFDGvec(elKmat, M, Ne, h, T, dt, p, 2);
       dfdG3vec(:, p) = formDFDGvec(elKmat, M, Ne, h, T, dt, p, 3);
    end
-   dOdGvec = formDODGvec(Lvec, dFdG1vec, dFdG2vec, dFdG3vec);
+   % STEP 4 compute gradient DODg (dOdg is zero in this case)
+   dOdGvec = formDODGvec(Lvec, dfdG1vec, dfdG2vec, dfdG3vec);
    gradOnorm = sqrt((dOdGvec')*dOdGvec);
    if gradOnorm <= 1.0e-12
       disp('Norm of the gradient is below the tolerance!') 
    else   
+     % BFGS Iteration 
       for iter = 1:maxOptIters
           dG = -invHess*dOdGvec;
           alpha = 1.0;
@@ -104,7 +119,7 @@ else
               dfdG2vec(:, p) = formDFDGvec(elKmat, M, Ne, h, T, dt, p, 2);
               dfdG3vec(:, p) = formDFDGvec(elKmat, M, Ne, h, T, dt, p, 3);
           end
-          dOdGvecNew = formDODGvec(Lvec, dFdG1vec, dFdG2vec, dFdG3vec);
+          dOdGvecNew = formDODGvec(Lvec, dfdG1vec, dfdG2vec, dfdG3vec);
           yVec = dOdGvecNew - dOdGvec;
           dOdGvec = dOdGvecNew;
           gradOnorm = sqrt((dOdGvec')*dOdGvec);
